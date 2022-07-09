e0e095ba2c53303677164755381ae51c815ca0ac
Paola gutierrez <paolagutierzgil@hotmail.com>
2022-07-09 18:47:30 -0500

    modificaciones en los padding

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,23 +1,17 @@
-import { Box,Grommet } from "grommet";
-import { grommet as light } from 'grommet/themes'
+import { Box, Grommet } from "grommet";
+import { grommet as light } from "grommet/themes";
+import { Header, Main } from "grommet";
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-const Page = (props) => {
+const Page = ({ children }) => {
   return (
-    <Grommet theme={light} full>
-    <Box align='center' flex={true} justify="center" pad='small' direction='column'>
-    <Navbar/>
-      <Box
-        border={{ color: "brand", size: "large" }}>
-        {props.children}
-      </Box>
+    <Box flex={true} pad="xsmall" direction="column" align="center">
+      <Navbar></Navbar>
+      <Main pad="small"> {children}</Main>
       <Footer></Footer>
-      </Box>
-      </Grommet>
-
-  
+    </Box>
   );
 };
 
