35751152f407da39c38d2d8603fab9012c13f02a
Paola gutierrez <paolagutierzgil@hotmail.com>
2022-07-11 20:04:04 -0500

    se agrego componente product a el menu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Page from "../components/Page";
 import { Box } from "grommet";
-import ProductCard from "../components/ProductCard";
+import Product from "../components/Product";
 
-const Menu = (props) => {
+const Menu = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(async () => {
+    const response = await axios.get("products");
+
+    setProducts(response.data);
+  }, []);
+
   return (
     <Page>
       <Box>
-        <ProductCard></ProductCard>
+        {products.map((product, i) => (
+          <Product
+            key={i}
+            image={product.image}
+            name={product.name}
+            price={product.price}
+            description={product.description}
+            id={product._id}
+          />
+        ))}
       </Box>
     </Page>
   );
